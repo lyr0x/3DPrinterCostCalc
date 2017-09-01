c3efb933120c038348390f4b09ff035b5ab9050d
lars <lars1107@outlook.com>
2017-09-01 10:45:14 +0200

    Namensgebung

--- 3D Print Cost Calc/3D Print Cost Calc/Form1.Designer.vb
+++ 3D Print Cost Calc/3D Print Cost Calc/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class CostCalc
     Inherits System.Windows.Forms.Form
 
     'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
@@ -25,44 +25,44 @@ Partial Class Form1
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NUDMaterialMeter = New System.Windows.Forms.NumericUpDown()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.tbPrinterName = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NUDMaterialCost = New System.Windows.Forms.NumericUpDown()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
-        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.NUDEnergyCost = New System.Windows.Forms.NumericUpDown()
+        Me.NUDEnergyKWH = New System.Windows.Forms.NumericUpDown()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.NumericUpDown5 = New System.Windows.Forms.NumericUpDown()
-        Me.NumericUpDown6 = New System.Windows.Forms.NumericUpDown()
+        Me.NUDWearCost = New System.Windows.Forms.NumericUpDown()
+        Me.NUDWearH = New System.Windows.Forms.NumericUpDown()
         Me.GroupBox4 = New System.Windows.Forms.GroupBox()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
-        Me.NumericUpDown7 = New System.Windows.Forms.NumericUpDown()
-        Me.NumericUpDown8 = New System.Windows.Forms.NumericUpDown()
+        Me.NUDWorkTimeCost = New System.Windows.Forms.NumericUpDown()
+        Me.NUDWorkTimeH = New System.Windows.Forms.NumericUpDown()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.TabControl1.SuspendLayout()
         Me.TabPage2.SuspendLayout()
-        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUDMaterialMeter, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
-        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUDMaterialCost, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox2.SuspendLayout()
-        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUDEnergyCost, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUDEnergyKWH, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox3.SuspendLayout()
-        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUDWearCost, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUDWearH, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox4.SuspendLayout()
-        CType(Me.NumericUpDown7, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUDWorkTimeCost, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NUDWorkTimeH, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TabControl1
@@ -93,7 +93,7 @@ Partial Class Form1
         Me.TabPage2.Controls.Add(Me.GroupBox3)
         Me.TabPage2.Controls.Add(Me.GroupBox2)
         Me.TabPage2.Controls.Add(Me.GroupBox1)
-        Me.TabPage2.Controls.Add(Me.TextBox1)
+        Me.TabPage2.Controls.Add(Me.tbPrinterName)
         Me.TabPage2.Controls.Add(Me.Label1)
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
@@ -103,19 +103,19 @@ Partial Class Form1
         Me.TabPage2.Text = "Settings"
         Me.TabPage2.UseVisualStyleBackColor = True
         '
-        'NumericUpDown1
+        'NUDMaterialMeter
         '
-        Me.NumericUpDown1.Location = New System.Drawing.Point(49, 19)
-        Me.NumericUpDown1.Name = "NumericUpDown1"
-        Me.NumericUpDown1.Size = New System.Drawing.Size(52, 20)
-        Me.NumericUpDown1.TabIndex = 3
+        Me.NUDMaterialMeter.Location = New System.Drawing.Point(49, 19)
+        Me.NUDMaterialMeter.Name = "NUDMaterialMeter"
+        Me.NUDMaterialMeter.Size = New System.Drawing.Size(52, 20)
+        Me.NUDMaterialMeter.TabIndex = 3
         '
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Controls.Add(Me.NumericUpDown2)
-        Me.GroupBox1.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox1.Controls.Add(Me.NUDMaterialCost)
+        Me.GroupBox1.Controls.Add(Me.NUDMaterialMeter)
         Me.GroupBox1.Location = New System.Drawing.Point(11, 39)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(238, 51)
@@ -123,12 +123,12 @@ Partial Class Form1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Material"
         '
-        'TextBox1
+        'tbPrinterName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(88, 13)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(161, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.tbPrinterName.Location = New System.Drawing.Point(88, 13)
+        Me.tbPrinterName.Name = "tbPrinterName"
+        Me.tbPrinterName.Size = New System.Drawing.Size(161, 20)
+        Me.tbPrinterName.TabIndex = 1
         '
         'Label1
         '
@@ -139,12 +139,12 @@ Partial Class Form1
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Druckername:"
         '
-        'NumericUpDown2
+        'NUDMaterialCost
         '
-        Me.NumericUpDown2.Location = New System.Drawing.Point(155, 19)
-        Me.NumericUpDown2.Name = "NumericUpDown2"
-        Me.NumericUpDown2.Size = New System.Drawing.Size(49, 20)
-        Me.NumericUpDown2.TabIndex = 4
+        Me.NUDMaterialCost.Location = New System.Drawing.Point(155, 19)
+        Me.NUDMaterialCost.Name = "NUDMaterialCost"
+        Me.NUDMaterialCost.Size = New System.Drawing.Size(49, 20)
+        Me.NUDMaterialCost.TabIndex = 4
         '
         'Label2
         '
@@ -168,8 +168,8 @@ Partial Class Form1
         '
         Me.GroupBox2.Controls.Add(Me.Label4)
         Me.GroupBox2.Controls.Add(Me.Label5)
-        Me.GroupBox2.Controls.Add(Me.NumericUpDown3)
-        Me.GroupBox2.Controls.Add(Me.NumericUpDown4)
+        Me.GroupBox2.Controls.Add(Me.NUDEnergyCost)
+        Me.GroupBox2.Controls.Add(Me.NUDEnergyKWH)
         Me.GroupBox2.Location = New System.Drawing.Point(11, 96)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Size = New System.Drawing.Size(238, 51)
@@ -195,26 +195,26 @@ Partial Class Form1
         Me.Label5.TabIndex = 5
         Me.Label5.Text = "KwH:"
         '
-        'NumericUpDown3
+        'NUDEnergyCost
         '
-        Me.NumericUpDown3.Location = New System.Drawing.Point(155, 19)
-        Me.NumericUpDown3.Name = "NumericUpDown3"
-        Me.NumericUpDown3.Size = New System.Drawing.Size(49, 20)
-        Me.NumericUpDown3.TabIndex = 4
+        Me.NUDEnergyCost.Location = New System.Drawing.Point(155, 19)
+        Me.NUDEnergyCost.Name = "NUDEnergyCost"
+        Me.NUDEnergyCost.Size = New System.Drawing.Size(49, 20)
+        Me.NUDEnergyCost.TabIndex = 4
         '
-        'NumericUpDown4
+        'NUDEnergyKWH
         '
-        Me.NumericUpDown4.Location = New System.Drawing.Point(49, 19)
-        Me.NumericUpDown4.Name = "NumericUpDown4"
-        Me.NumericUpDown4.Size = New System.Drawing.Size(52, 20)
-        Me.NumericUpDown4.TabIndex = 3
+        Me.NUDEnergyKWH.Location = New System.Drawing.Point(49, 19)
+        Me.NUDEnergyKWH.Name = "NUDEnergyKWH"
+        Me.NUDEnergyKWH.Size = New System.Drawing.Size(52, 20)
+        Me.NUDEnergyKWH.TabIndex = 3
         '
         'GroupBox3
         '
         Me.GroupBox3.Controls.Add(Me.Label6)
         Me.GroupBox3.Controls.Add(Me.Label7)
-        Me.GroupBox3.Controls.Add(Me.NumericUpDown5)
-        Me.GroupBox3.Controls.Add(Me.NumericUpDown6)
+        Me.GroupBox3.Controls.Add(Me.NUDWearCost)
+        Me.GroupBox3.Controls.Add(Me.NUDWearH)
         Me.GroupBox3.Location = New System.Drawing.Point(11, 153)
         Me.GroupBox3.Name = "GroupBox3"
         Me.GroupBox3.Size = New System.Drawing.Size(238, 51)
@@ -240,26 +240,26 @@ Partial Class Form1
         Me.Label7.TabIndex = 5
         Me.Label7.Text = "Stunde:"
         '
-        'NumericUpDown5
+        'NUDWearCost
         '
-        Me.NumericUpDown5.Location = New System.Drawing.Point(155, 19)
-        Me.NumericUpDown5.Name = "NumericUpDown5"
-        Me.NumericUpDown5.Size = New System.Drawing.Size(49, 20)
-        Me.NumericUpDown5.TabIndex = 4
+        Me.NUDWearCost.Location = New System.Drawing.Point(155, 19)
+        Me.NUDWearCost.Name = "NUDWearCost"
+        Me.NUDWearCost.Size = New System.Drawing.Size(49, 20)
+        Me.NUDWearCost.TabIndex = 4
         '
-        'NumericUpDown6
+        'NUDWearH
         '
-        Me.NumericUpDown6.Location = New System.Drawing.Point(49, 19)
-        Me.NumericUpDown6.Name = "NumericUpDown6"
-        Me.NumericUpDown6.Size = New System.Drawing.Size(52, 20)
-        Me.NumericUpDown6.TabIndex = 3
+        Me.NUDWearH.Location = New System.Drawing.Point(49, 19)
+        Me.NUDWearH.Name = "NUDWearH"
+        Me.NUDWearH.Size = New System.Drawing.Size(52, 20)
+        Me.NUDWearH.TabIndex = 3
         '
         'GroupBox4
         '
         Me.GroupBox4.Controls.Add(Me.Label8)
         Me.GroupBox4.Controls.Add(Me.Label9)
-        Me.GroupBox4.Controls.Add(Me.NumericUpDown7)
-        Me.GroupBox4.Controls.Add(Me.NumericUpDown8)
+        Me.GroupBox4.Controls.Add(Me.NUDWorkTimeCost)
+        Me.GroupBox4.Controls.Add(Me.NUDWorkTimeH)
         Me.GroupBox4.Location = New System.Drawing.Point(11, 210)
         Me.GroupBox4.Name = "GroupBox4"
         Me.GroupBox4.Size = New System.Drawing.Size(238, 51)
@@ -285,19 +285,19 @@ Partial Class Form1
         Me.Label9.TabIndex = 5
         Me.Label9.Text = "Stunde:"
         '
-        'NumericUpDown7
+        'NUDWorkTimeCost
         '
-        Me.NumericUpDown7.Location = New System.Drawing.Point(155, 19)
-        Me.NumericUpDown7.Name = "NumericUpDown7"
-        Me.NumericUpDown7.Size = New System.Drawing.Size(49, 20)
-        Me.NumericUpDown7.TabIndex = 4
+        Me.NUDWorkTimeCost.Location = New System.Drawing.Point(155, 19)
+        Me.NUDWorkTimeCost.Name = "NUDWorkTimeCost"
+        Me.NUDWorkTimeCost.Size = New System.Drawing.Size(49, 20)
+        Me.NUDWorkTimeCost.TabIndex = 4
         '
-        'NumericUpDown8
+        'NUDWorkTimeH
         '
-        Me.NumericUpDown8.Location = New System.Drawing.Point(49, 19)
-        Me.NumericUpDown8.Name = "NumericUpDown8"
-        Me.NumericUpDown8.Size = New System.Drawing.Size(52, 20)
-        Me.NumericUpDown8.TabIndex = 3
+        Me.NUDWorkTimeH.Location = New System.Drawing.Point(49, 19)
+        Me.NUDWorkTimeH.Name = "NUDWorkTimeH"
+        Me.NUDWorkTimeH.Size = New System.Drawing.Size(52, 20)
+        Me.NUDWorkTimeH.TabIndex = 3
         '
         'Button1
         '
@@ -317,33 +317,33 @@ Partial Class Form1
         Me.Button2.Text = "Load"
         Me.Button2.UseVisualStyleBackColor = True
         '
-        'Form1
+        'CostCalc
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(495, 415)
         Me.Controls.Add(Me.TabControl1)
-        Me.Name = "Form1"
+        Me.Name = "CostCalc"
         Me.Text = "3D Printer Cost Calculator"
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage2.ResumeLayout(False)
         Me.TabPage2.PerformLayout()
-        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUDMaterialMeter, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
-        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUDMaterialCost, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
-        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUDEnergyCost, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUDEnergyKWH, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox3.ResumeLayout(False)
         Me.GroupBox3.PerformLayout()
-        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUDWearCost, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUDWearH, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox4.ResumeLayout(False)
         Me.GroupBox4.PerformLayout()
-        CType(Me.NumericUpDown7, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUDWorkTimeCost, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NUDWorkTimeH, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -351,28 +351,28 @@ Partial Class Form1
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents TabPage2 As TabPage
-    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents NUDMaterialMeter As NumericUpDown
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents tbPrinterName As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Button2 As Button
     Friend WithEvents Button1 As Button
     Friend WithEvents GroupBox4 As GroupBox
     Friend WithEvents Label8 As Label
     Friend WithEvents Label9 As Label
-    Friend WithEvents NumericUpDown7 As NumericUpDown
-    Friend WithEvents NumericUpDown8 As NumericUpDown
+    Friend WithEvents NUDWorkTimeCost As NumericUpDown
+    Friend WithEvents NUDWorkTimeH As NumericUpDown
     Friend WithEvents GroupBox3 As GroupBox
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
-    Friend WithEvents NumericUpDown5 As NumericUpDown
-    Friend WithEvents NumericUpDown6 As NumericUpDown
+    Friend WithEvents NUDWearCost As NumericUpDown
+    Friend WithEvents NUDWearH As NumericUpDown
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
-    Friend WithEvents NumericUpDown3 As NumericUpDown
-    Friend WithEvents NumericUpDown4 As NumericUpDown
+    Friend WithEvents NUDEnergyCost As NumericUpDown
+    Friend WithEvents NUDEnergyKWH As NumericUpDown
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents NUDMaterialCost As NumericUpDown
 End Class
--- 3D Print Cost Calc/3D Print Cost Calc/Form1.vb
+++ 3D Print Cost Calc/3D Print Cost Calc/Form1.vb
@@ -1,3 +1,6 @@
-﻿Public Class Form1
+﻿Public Class CostCalc
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+
+    End Sub
 End Class
